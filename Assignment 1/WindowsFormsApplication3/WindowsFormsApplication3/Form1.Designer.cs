3e69820e8267b2a7a7ae3fdc48b0b527791b567c
mert ertugrul m <mert.ertuurul@gmail.com>
2016-05-28 17:13:25 +0300

    dotNET application development
    Assignment 1 - guess the number

---
+++ Assignment 1/WindowsFormsApplication3/WindowsFormsApplication3/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace WindowsFormsApplication3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(77, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Please make your first guess. ";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(80, 156);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Try !";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.textBox1.Location = new System.Drawing.Point(80, 63);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(97, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(77, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Too low, go higher !";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(77, 31);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(124, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Number between 1-1000";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(305, 260);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Guess My Number";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+
+        
+    }
+}
+
---
+++ Assignment 1/WindowsFormsApplication3/WindowsFormsApplication3/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+
+
+namespace WindowsFormsApplication3
+{
+     
+
+    public partial class Form1 : Form
+    {
+       int thenumber=5;
+       int guess;
+       int choosenbutton;
+
+  
+        private void GenerateRandoms()
+{
+    // create random number variable
+    Random random = new Random();
+    thenumber = random.Next(0, 1000);
+            
+        } 
+
+        public Form1()
+        {
+            InitializeComponent();
+            GenerateRandoms();
+            
+
+                    
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+           // label1.Text = thenumber.ToString();
+           // label2.Text = guess.ToString();
+           Int32.TryParse(textBox1.Text, out guess);
+
+           if (guess > thenumber)
+               label2.Text = "It's high, get lower !";
+           
+           if (guess < thenumber)
+               label2.Text = "It's low, get higher !";
+
+           if (guess == thenumber)
+           {
+               label2.Text = "Correct !";
+               this.BackColor = System.Drawing.Color.Green;
+               textBox1.Enabled = false;
+
+               DialogResult choosenbutton = MessageBox.Show("Do you want to play again?","Game Ended", MessageBoxButtons.YesNo);
+               if (choosenbutton == DialogResult.Yes)
+                   Application.Restart();
+               else
+                   Application.Exit();
+           }
+
+           if (guess > thenumber + 100 || guess < thenumber - 100 )
+           {
+               textBox1.BackColor = Color.Turquoise;
+                         }
+               else
+           {
+               textBox1.BackColor =  Color.Red;
+           
+}
+           
+               
+
+
+           
+
+            
+           
+
+
+        }
+
+
+    
+    }
+}
